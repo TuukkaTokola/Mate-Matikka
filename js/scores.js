5ad8bd4e884f67b8c29a50fad9cf0a2cff976677
Miko Hietala <mihietal25@students.oamk.fi>
2026-04-28 12:49:08 +0300

    Updated peli 5

--- js/scores.js
+++ js/scores.js
@@ -20,3 +20,27 @@ function setScore(index, value) {
         console.error("Index out of bounds");
     }
 }
+function showScores() {
+  loadScores();
+
+  const scoreList = document.getElementById("score-list");
+  const totalScore = document.getElementById("total-score");
+
+  if (!scoreList || !totalScore) {
+    return;
+  }
+
+  scoreList.innerHTML = `
+    <p>Peli 1: ${scores[0]}/10</p>
+    <p>Peli 2: ${scores[1]}/10</p>
+    <p>Peli 3: ${scores[2]}/10</p>
+    <p>Peli 4: ${scores[3]}/10</p>
+    <p>Peli 5: ${scores[4]}/10</p>
+    <p>Peli 6: ${scores[5]}/10</p>
+  `;
+
+  const total = scores.reduce((sum, value) => sum + Number(value), 0);
+  totalScore.textContent = total;
+}
+
+showScores();
